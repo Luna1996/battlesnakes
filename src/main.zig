ea6bd57e598827853a2afecc063f3024677eb3a4
YifuWang <wyf19960926@gmail.com>
2025-02-11 09:22:43 +0000

    snake born

--- src/main.zig
+++ src/main.zig
@@ -6,6 +6,16 @@ const log = @import("logs.zig");
 
 pub usingnamespace log;
 
+const PORT = 8888;
+const INFO = .{
+  .apiversion = "1",
+  .author = "Luna1996",
+  .color = "#ffd700",
+  .head = "pixel",
+  .tail = "pixel",
+  .version = "0.1.0",
+};
+
 allocator: std.mem.Allocator,
 
 pub fn init(allocator: std.mem.Allocator) Self {
@@ -18,9 +28,21 @@ pub fn main() !void {
   var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
   defer _ = gpa.deinit();
   defer _ = gpa.detectLeaks();
-  // const allocator = gpa.allocator();
+  const allocator = gpa.allocator();
 
-  // var app = init(allocator);
+  var app = init(allocator);
 
-  // var server = try
+  var server = try web.Server(*Self).init(allocator, .{.port = PORT}, &app);
+  defer server.deinit();
+
+  var router = server.router(.{});
+  router.get("/", onPing, .{});
+
+  try server.listen();
+  defer server.stop();
+}
+
+pub fn onPing(_: *Self, _: *web.Request, res: *web.Response) !void {
+  res.status = 200;
+  try res.json(INFO, .{});
 }

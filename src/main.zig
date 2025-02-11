1be0e503ddf35b77ac04646baf35089b81a5f18c
YifuWang <wyf19960926@gmail.com>
2025-02-11 04:54:10 +0000

    setup http server

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,19 @@
 const std = @import("std");
+const zap = @import("zap");
 
 pub usingnamespace @import("logs.zig");
 
 pub fn main() !void {
-  std.log.info("Battle Snakes\n", .{});
+  var server = zap.HttpListener.init(.{
+    .port = 8888,
+    .on_request = onRequest,
+  });
+  try server.listen();
+
+  zap.start(.{.threads = 1, .workers = 1});
+}
+
+fn onRequest(req: zap.Request) void {
+  req.setContentType(.HTML) catch |e| std.log.err("{}\n", .{e});
+  req.sendBody("<html><body><h1>Battle Snakes</h1></body></html>") catch |e| std.log.err("{}\n", .{e});
 }
